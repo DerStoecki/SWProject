1fe0543d5762ec853d647067511e124175e1e6ec
Felix <felix.stoecker@googlemail.com>
2020-10-27 16:21:40 +0100

    Init Files

---
+++ WatchTHot-starter/src/main/java/de/othr/sw/WatchTHot/WatchTHotstarter/WatchTHotStarterApplication.java
@@ -0,0 +1,13 @@
+package de.othr.sw.WatchTHot.WatchTHotstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchTHotStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchTHotStarterApplication.class, args);
+	}
+
+}
---
+++ WatchTHot-starter/src/test/java/de/othr/sw/WatchTHot/WatchTHotstarter/WatchTHotStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.othr.sw.WatchTHot.WatchTHotstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchTHotStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
